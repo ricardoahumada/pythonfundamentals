b2894ff9e0f3e55083894b9a4533558d0b425851
ricardoag <ricardo@enmotionvalue.com>
2025-02-12 07:45:26 +0100

    update

--- sql/env.py
+++ sql/env.py
@@ -1,6 +1,6 @@
 # Do not expose your Neon credentials to the browser
 
-PGHOST='ep-super-meadow-a2w5lasv.eu-central-1.aws.neon.tech'
-PGDATABASE='datafundamentasl'
-PGUSER='datafundamentasl_owner'
-PGPASSWORD='yPDtEpT5R6mI'
+PGHOST='PGHOST'
+PGDATABASE='PGDATABASE'
+PGUSER='PGUSER'
+PGPASSWORD='PGPASSWORD'
